e69dd9d479afa5bfc21257716552b82b58f31d54
ZohaibFattani <fattanizohaib@gmail.com>
2022-10-25 20:39:07 -0700

    added styles through variable

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import './App.css';
+import "./style"
+
+const headerStyle = {
+  fontSize: "50px",
+  textAlign: "center",
+  margin: "40px",
+  border: "10px solid red",
+};
+
+const styleText = {
+  fontSize: "25px",
+  textAlign: "center",
+  border: "5px solid blue",
+}
+
+const formStyle = {
+  fontSize: "25px",
+  border: "5px solid green",
+}
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1 style={headerStyle}>Welcome to My Portfolio</h1>
+      <p style={styleText}>My Favorite Foods</p>
+      <form style={formStyle}>
+        <ol>
+          <li>Pizza</li>
+          <li>Burger</li>
+          <li>Cookie</li>
+        </ol>
+      </form>
     </div>
   );
 }
---
+++ src/style.js
@@ -0,0 +1,6 @@
+const headerStyle = {
+  fontSize: "25px",
+  textAlign: "center",
+  margin: "40px",
+  border: "10px solid red",
+};
